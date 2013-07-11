c57d4aefb0918a1630683e5b63987e1ded0753dc
Javier Olaechea <pirata@gmail.com>
2013-07-11 02:18:09 -0400

    Enchance clojure mode with purcell's config

--- user-lisp/setup-clojure-mode.el
+++ user-lisp/setup-clojure-mode.el
@@ -1,22 +1,34 @@
 ;; Rainbow parentesis
-(require 'rainbow-delimiters)
+(require 'clojure-mode)
+(require 'clojure-test-mode)
+(require 'cljsbuild-mode)
+(require 'elein)
 (require 'nrepl)
+(require 'slamhound)
 (require 'ac-nrepl)
+
 
 (setq nrepl-popup-stacktraces-in nil
       nrepl-hide-special-buffers t
       nrepl-popup-stacktraces-in-repl t
       nrepl-history-file "~/.emacs.d/nrepl-history")
 
-;; Eldoc
-(add-hook 'clojure-mode-hook 'turn-on-eldoc-mode)
-(add-hook 'nrepl-interaction-mode-hook 'nrepl-turn-on-eldoc-mode)
-(add-hook 'nrepl-connected-hook 'nrepl-enable-on-existing-clojure-buffers)
+(after-load 'nrepl
+            (add-hook 'nrepl-mode-hook 'ac-nrepl-setup)
+            (add-hook 'nrepl-interaction-mode-hook 'ac-nrepl-setup)
+            (add-hook 'nrepl-mode-hook 'paredit-mode)
+            (add-hook 'nrepl-mode-hook 'subword-mode)
+            (add-hook 'nrepl-interaction-mode-hook 'nrepl-turn-on-eldoc-mode)
+            (add-hook 'nrepl-connected-hook 'nrepl-enable-on-existing-clojure-buffers)
+            (define-key nrepl-interaction-mode-map (kbd "C-c C-d")
+              'ac-nrepl-popup-doc)
+)
 
-(add-hook 'nrepl-mode-hook 'paredit-mode)
-(add-hook 'nrepl-mode-hook 'rainbow-delimiters-mode)
-(add-hook 'nrepl-mode-hook 'subword-mode)
-(add-hook 'nrepl-mode-hook 'ac-nrepl-setup)
+(after-load  'clojure-mode
+             (add-hook 'clojure-mode-hook 'subword-mode)
+             (add-hook 'clojure-mode-hook 'turn-on-eldoc-mode))
+
+(add-auto-mode 'clojure-mode "\\.cljs\\'")
 
 ; depends also en ac-mode
 (add-hook 'clojure-nrepl-mode-hook 'ac-nrepl-setup)
