125f239a399387c7c0c0e976f2806736fc18850a
Javier Olaechea <pirata@gmail.com>
2013-07-11 03:48:02 -0400

    Replace Append-to-kill with an backward-kill-sentence

--- user-lisp/key-bindings.el
+++ user-lisp/key-bindings.el
@@ -17,4 +17,6 @@
 (global-set-key (kbd "C-M-s") 'isearch-forward)
 (global-set-key (kbd "C-M-r") 'isearch-backward)
 
+(global-set-key (kbd "C-M-w") 'backward-kill-sentence)
+
 (provide 'key-bindings)
