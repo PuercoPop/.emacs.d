3c59074cd4c4e8b80616bb3812720d74b8ae3cc0
Javier Olaechea <pirata@gmail.com>
2013-11-04 15:30:27 -0500

    Setup jedi and virtualenv support

--- user-lisp/setup-python.el
+++ user-lisp/setup-python.el
@@ -1,12 +1,23 @@
+(require 'virtualenvwrapper)
+(setq venv-location "~/.envs/")
+(venv-initialize-eshell)
+(venv-initialize-interactive-shells)
+
+(setq jedi:setup-keys t
+      jedi:complete-on-dot t)
+
 (add-hook 'python-mode-hook
           '(lambda ()
              (setq fill-column 79)
              (add-hook 'before-save-hook 'delete-trailing-whitespace)
-             (setq ac-sources '(ac-source-python))
              (define-key python-mode-map (kbd "<f5>") 'pep8)
              (font-lock-add-keywords
               nil
               '(("\\<\\(FIXME\\|TODO\\|BUG\\):" 1 font-lock-warning-face t)))
-             ))
+             (jedi:setup)
+             (jedi:ac-setup)))
+
+(eval-after-load "python"
+  '(define-key python-mode-map "\C-cx" 'jedi-direx:pop-to-buffer))
 
 (provide 'setup-python)
