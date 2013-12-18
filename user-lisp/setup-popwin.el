dd810cfa5ba367957553399a914ba0f99e50e7d1
PuercoPop <puercopop@Admins-MacBook-Pro.local>
2013-12-18 11:24:23 -0500

    Remove slime's repl from popwin

--- user-lisp/setup-popwin.el
+++ user-lisp/setup-popwin.el
@@ -1,6 +1,8 @@
 (require 'popwin)
 (popwin-mode 1)
 
+(setq popwin:special-display-config
+      (cl-remove 'slime-repl-mode popwin:special-display-config))
 (add-to-list 'popwin:special-display-config "*python-pep8*")
 (push '("*Agenda Commands*" :height 30) popwin:special-display-config)
 
