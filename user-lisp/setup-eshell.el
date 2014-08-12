af8f5011cb0fa98ae4680a3635e353fdc903aed0
Javier Olaechea <pirata@gmail.com>
2014-08-12 13:01:36 -0500

    Eshell at the tip of my fingers

--- user-lisp/setup-eshell.el
+++ user-lisp/setup-eshell.el
@@ -2,4 +2,20 @@
 
 (add-hook 'eshell-load-hook 'nyan-prompt-enable)
 
+;; press F11 to bring up the eshell buffer if no eshell buffer is open then open one
+
+(defun ppop-bring-up-eshell ()
+  (interactive)
+  (if (string= "eshell-mode" major-mode)
+      (jump-to-register :eshell-fullscreen)
+    (let ((eshell-buffer (or (get-buffer "*eshell*")
+                               (eshell))))
+        (window-configuration-to-register :eshell-fullscreen)
+        (switch-to-buffer eshell-buffer))))
+
+
+(global-set-key [f11] 'ppop-bring-up-eshell)
+
+
+
 (provide 'setup-eshell)
