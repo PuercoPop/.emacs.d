4b794ba50c3e0e95e295dbd9431678f2bf893408
Javier Olaechea <pirata@gmail.com>
2015-02-16 00:24:38 -0500

    Add mc/mark-all-like-this-dwim to C-*

--- user-lisp/setup-mark-multiple.el
+++ user-lisp/setup-mark-multiple.el
@@ -1,9 +1,10 @@
-(global-set-key (kbd "C-x r t") 'inline-string-rectangle)
 
-(global-set-key (kbd "C-<") 'mark-previous-like-this)
-(global-set-key (kbd "C->") 'mark-next-like-this)
+;; (global-set-key (kbd "C-x r t") 'inline-string-rectangle)
+
+(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+(global-set-key (kbd "C->") 'mc/mark-next-like-this)
 (global-set-key (kbd "C-M-m") 'mark-more-like-this) ; like the other two, but takes an argument (negative is previous)
-(global-set-key (kbd "C-*") 'mark-all-like-this)
+(global-set-key (kbd "C-*") 'mc/mark-all-like-this-dwim)
 ;; (require 'key-chord)
 ;; (key-chord-define-global "mn" 'mark-previous-like-this)
 ;; (key-chord-define-global "m," 'mark-previous-like-this)
@@ -15,4 +16,4 @@
             (require 'rename-sgml-tag)
             (define-key sgml-mode-map (kbd "C-c C-r") 'mc/mark-sgml-tag-pair)))
 
-(provide 'setup-mark-multiple)
+(provide 'setup-multiple-cursors)
