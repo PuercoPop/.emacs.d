9d44da761a4ac348d146d4411c9230f1ad5ffa8e
Javier Olaechea <pirata@gmail.com>
2015-07-22 17:43:55 -0500

    change default modeline

--- user-lisp/misc-settings.el
+++ user-lisp/misc-settings.el
@@ -112,4 +112,7 @@
 
 (setq visible-bell t)
 
+(setq-default mode-line-format
+              '("%e" mode-line-front-space mode-line-mule-info mode-line-client mode-line-modified mode-line-remote mode-line-frame-identification mode-line-buffer-identification "   " mode-line-position (vc-mode vc-mode) "  " mode-name " " mode-line-misc-info mode-line-end-spaces))
+
 (provide 'misc-settings)
