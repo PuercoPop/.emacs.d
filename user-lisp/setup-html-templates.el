053ea053447fff62f7eed1e6a39e005b4a290434
Javier Olaechea <pirata@gmail.com>
2015-09-07 01:23:47 -0500

    Remove ac-html from html-mode

--- user-lisp/setup-html-templates.el
+++ user-lisp/setup-html-templates.el
@@ -1,8 +1,6 @@
 (add-to-list 'auto-mode-alist '("\\.tmpl\\'" . closure-template-html-mode))
 (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . jinja2-mode))
 
-(add-hook 'html-mode-hook 'ac-html-enable)
-
 (require 'handlebars-sgml-mode)
 (handlebars-use-mode 'global)
 
