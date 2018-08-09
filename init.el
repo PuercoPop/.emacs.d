5f56fd679254caba1036a5633aa12698d54df53e
Javier Olaechea <pirata@gmail.com>
2018-08-09 08:09:17 -0500

    Move smtpmail to use-package

--- init.el
+++ init.el
@@ -248,6 +248,16 @@
 (require 'setup-cl-mode)
 (require 'setup-sly)
 
+(use-package smtpmail
+  :config (setq smtpmail-smtp-server "smtp.gmail.com"
+                smtpmail-local-domain "gmail.com"
+                smtpmail-sendto-domain "gmail.com"
+                smtpmail-smtp-user "pirata@gmail.com"
+                smtpmail-smtp-service 465
+                smtpmail-stream-type 'ssl
+                smtpmail-debug-info t
+                send-mail-function 'smtpmail-send-it))
+
 ;;; Magit
 (require 'setup-magit)
 (fullframe magit-status magit-mode-quit-window)
--- user-lisp/setup-smtp.el
+++
@@ -1,9 +0,0 @@
-(setq smtpmail-smtp-server "smtp.gmail.com"
-      smtpmail-local-domain "gmail.com"
-      smtpmail-sendto-domain "gmail.com"
-      smtpmail-smtp-service 465
-      smtpmail-stream-type 'ssl
-      smtpmail-debug-info t
-      send-mail-function 'smtpmail-send-it)
-
-(provide 'setup-smtp)
