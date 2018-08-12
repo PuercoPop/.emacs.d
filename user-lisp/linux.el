d4d8b3b102325cc045f896e3b091a4579e6eff55
Javier Olaechea <pirata@gmail.com>
2018-08-11 23:28:55 -0500

    Cleanup fonts

--- user-lisp/linux.el
+++ user-lisp/linux.el
@@ -1,10 +1,15 @@
 ;; (condition-case nil
-;;     (set-face-attribute 'default nil :font "Dejavu Sans Mono-11")
+;;    (set-face-attribute 'default nil :font "Dejavu Sans Mono-13")
 ;;   (error "No such font"))
 
+;; "Fantasque Sans Mono-13"
+;; "Dejavu Sans Mono-11"
+;; "Operator Mono-18"
+;; "Input Serif-11"
+;; "Terminus-13"
+
 (condition-case nil
-    (set-face-attribute 'default nil :font "Fantasque Sans Mono-14")
+    (set-face-attribute 'default nil :font "Fantasque Sans Mono-13")
   (error "No such font"))
-;; (set-face-attribute 'default nil :font "Input Serif-11")
 
 (provide 'linux)
