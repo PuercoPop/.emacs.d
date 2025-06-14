dd47d39d563e2c0e05e0cb91ca98fa3663a3a6cb
Javier Olaechea <pirata@gmail.com>
2025-06-14 14:34:42 -0500

    Add more tree-sitter grammars

--- init.el
+++ init.el
@@ -1605,7 +1605,20 @@ in."
 (use-package treesit
   :config
   (setq treesit-language-source-alist
-        '((rust "https://github.com/tree-sitter/tree-sitter-rust.git"))))
+        '((css "https://github.com/tree-sitter/tree-sitter-css")
+          (dockerfile "https://github.com/camdencheek/tree-sitter-dockerfile")
+          (go "https://github.com/tree-sitter/tree-sitter-go")
+          (gomod "https://github.com/camdencheek/tree-sitter-go-mod")
+          (hcl "https://github.com/MichaHoffmann/tree-sitter-hcl")
+          (html "https://github.com/tree-sitter/tree-sitter-html")
+          (javascript "https://github.com/tree-sitter/tree-sitter-javascript" "master" "src")
+          (json "https://github.com/tree-sitter/tree-sitter-json")
+          (ruby "https://github.com/tree-sitter/tree-sitter-ruby")
+          (rust "https://github.com/tree-sitter/tree-sitter-rust.git")
+          (toml "https://github.com/tree-sitter/tree-sitter-toml")
+          (tsx "https://github.com/tree-sitter/tree-sitter-typescript" "master" "tsx/src")
+          (typescript "https://github.com/tree-sitter/tree-sitter-typescript" "master" "typescript/src")
+          (yaml "https://github.com/ikatyang/tree-sitter-yaml"))))
 
 (use-package rust-ts-mode
   :mode ("\\.rs\\'" . rust-ts-mode)
