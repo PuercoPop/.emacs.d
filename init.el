9a8db66894cd68f8b14d97f2911340e02ba64928
Javier Olaechea <pirata@gmail.com>
2025-07-04 10:25:47 -0500

    Enable org-capture

--- init.el
+++ init.el
@@ -719,7 +719,7 @@ And update the branch as a suffix."
           ;; (org-agenda-list)
           (org-agenda arg "w")))))
   :bind (("C-c a" . org-agenda)
-         ;; ("C-c c" . org-capture)
+         ("C-c c" . org-capture)
          ("C-c C-x C-x" . org-clock-in-last)
          ("C-c C-x C-o" . org-clock-out)
          ("C-c C-x C-j" . org-clock-goto)
